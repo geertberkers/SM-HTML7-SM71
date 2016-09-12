5d81bc9ec177df45852b08bcddfb938d9cb136a3
Geert Berkers <geertberkers@hotmail.com>
2016-09-12 12:35:33 +0200

    Again
    again

---
+++ KMTracker/Droid/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("KMTracker.Droid.MainApplication, KMTracker.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md50ba5caa860d90c94235dabed4e9f2af0.MainApplication.class, md50ba5caa860d90c94235dabed4e9f2af0.MainApplication.__md_methods);
+		
+	}
+}
